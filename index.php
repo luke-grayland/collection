185aeccc7fd5a622b233582fdc686c8243c6ccde
Luke Grayland <lgrayland96@gmail.com>
2022-02-07 15:30:48 +0000

    looped over db query result, outputted to html

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
 <?php
 
 include_once 'functions.php';
-
 $db = fetchDb();
-$result = dbQuery($db);
+$results = dbQuery($db);
 
-echo '<pre>';
-var_dump($result);
-echo '<pre>';
+foreach ($results as $result) {
+     echo "<div>
+            <h2>$result[album_name]</h2>
+            <h3>$result[artist_name]</h3>
+            <p>Year of Release: $result[year]</p>
+            <p>Rating: $result[rating]</p>
+            <img src='$result[cover]' alt='album cover' width='250px' height='250px'>
+            </div>";
+}
 
-?>
+
 

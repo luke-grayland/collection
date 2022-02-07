94396d9077f3585ee7951611ac677c42dae2d5fe
Luke Grayland <lgrayland96@gmail.com>
2022-02-07 12:00:55 +0000

    created files and export database with structure completed

---
+++ luke-collection.sql
@@ -0,0 +1,46 @@
+# ************************************************************
+# Sequel Ace SQL dump
+# Version 20025
+#
+# https://sequel-ace.com/
+# https://github.com/Sequel-Ace/Sequel-Ace
+#
+# Host: 127.0.0.1 (MySQL 5.7.37)
+# Database: luke-collection
+# Generation Time: 2022-02-07 11:56:58 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+SET NAMES utf8mb4;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE='NO_AUTO_VALUE_ON_ZERO', SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table luke-albums
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `luke-albums`;
+
+CREATE TABLE `luke-albums` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `album_name` varchar(255) NOT NULL DEFAULT '',
+  `artist_name` varchar(255) NOT NULL DEFAULT '',
+  `year` int(11) unsigned NOT NULL,
+  `rating` int(11) unsigned DEFAULT NULL,
+  `cover` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

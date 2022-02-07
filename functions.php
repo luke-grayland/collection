21b93c04bb0c0c6e020097a4863e18284f155227
Luke Grayland <lgrayland96@gmail.com>
2022-02-07 15:51:33 +0000

    migrated result echo process into a function

--- functions.php
+++ functions.php
@@ -11,5 +11,21 @@ function dbQuery($db) {
     return $query->fetchAll();
 }
 
+function readResults(array $results): string {
+    $resultsToReturn = '';
+
+    foreach ($results as $result) {
+        $result = "<div>
+            <h2>$result[album_name]</h2>
+            <h3>$result[artist_name]</h3>
+            <p>Year of Release: $result[year]</p>
+            <p>Rating: $result[rating]</p>
+            <img src='$result[cover]' alt='album cover' width='250px' height='250px'>
+            </div>";
+
+             $resultsToReturn .= $result;
+    }
+        return $resultsToReturn;
+}
 
 
--- index.php
+++ index.php
@@ -4,15 +4,6 @@ include_once 'functions.php';
 $db = fetchDb();
 $results = dbQuery($db);
 
-foreach ($results as $result) {
-     echo "<div>
-            <h2>$result[album_name]</h2>
-            <h3>$result[artist_name]</h3>
-            <p>Year of Release: $result[year]</p>
-            <p>Rating: $result[rating]</p>
-            <img src='$result[cover]' alt='album cover' width='250px' height='250px'>
-            </div>";
-}
-
+echo readResults($results);
 
 

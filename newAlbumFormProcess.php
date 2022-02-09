d1dafd302f608487e600061d8a5f852b3da00b00
Luke Grayland <lgrayland96@gmail.com>
2022-02-09 16:53:54 +0000

    started building form functionality

--- newAlbumFormProcess.php
+++ newAlbumFormProcess.php
@@ -1,12 +1,36 @@
 <?php
 
-echo "
-    Currently I'm just echoing this out to show you it's working... <br><br>
+require_once 'functions.php';
+
+$newAlbumData = $_POST;
+
+if (checkAlbumDataExists($newAlbumData)) {
+    //Sanitise
+    $newAlbumData['albumName'] = filter_var($newAlbumData['albumName'], FILTER_SANITIZE_STRING);
+    $newAlbumData['artistName'] = filter_var($newAlbumData['artistName'], FILTER_SANITIZE_STRING);
+    $newAlbumData['yearOfRelease'] = filter_var($newAlbumData['artistName'], FILTER_SANITIZE_NUMBER_INT);
+    $newAlbumData['albumArtworkURL'] = filter_var($newAlbumData['artistName'], FILTER_SANITIZE_URL);
+    $newAlbumData['rating'] = filter_var($newAlbumData['rating'], FILTER_SANITIZE_NUMBER_INT);
     
-    Album name: $_POST[albumName] <br>
-    Artist name: $_POST[artistName] <br>
-    Year: $_POST[yearOfRelease] <br>
-    URL: $_POST[albumArtworkURL] <br>
-    Rating: $_POST[rating] <br>
-    ";
-    
+
+    if (
+        //Validate (Some of this is truthy, some isn't - still trying to figure out why)
+        strlen($newAlbumData['albumName']) < 255 &&
+        strlen($newAlbumData['artistName']) < 255 &&
+        $newAlbumData['yearOfRelease'] > 1000 &&
+        $newAlbumData['yearOfRelease'] < 2022 &&
+        strlen($newAlbumData['albumArtworkURL']) < 255 &&
+        $newAlbumData['rating'] < 11 &&
+        $newAlbumData['rating'] > 0
+    ) {
+        /* Add to DB */
+
+        echo '<pre>';
+        var_dump($newAlbumData);
+        echo '<pre>';
+
+    } else {
+        echo 'error';
+    }
+}
+

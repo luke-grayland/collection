be34ea1b3f6a3730c1b5ec0d41a1c760eb8424d9
Luke Grayland <lgrayland96@gmail.com>
2022-02-10 14:19:31 +0000

    renamed variables to avoid mutating data

--- newAlbumFormProcess.php
+++ newAlbumFormProcess.php
@@ -6,27 +6,27 @@ $newAlbumData = $_POST;
 
 if (checkAlbumDataExists($newAlbumData)) {
     //Sanitise
-    $newAlbumData['albumName'] = filter_var($newAlbumData['albumName'], FILTER_SANITIZE_STRING);
-    $newAlbumData['artistName'] = filter_var($newAlbumData['artistName'], FILTER_SANITIZE_STRING);
-    $newAlbumData['yearOfRelease'] = filter_var($newAlbumData['yearOfRelease'], FILTER_SANITIZE_NUMBER_INT);
-    $newAlbumData['albumArtworkURL'] = filter_var($newAlbumData['albumArtworkURL'], FILTER_SANITIZE_URL);
-    $newAlbumData['rating'] = filter_var($newAlbumData['rating'], FILTER_SANITIZE_NUMBER_INT);
+    $albumName = filter_var($newAlbumData['albumName'], FILTER_SANITIZE_STRING);
+    $artistName = filter_var($newAlbumData['artistName'], FILTER_SANITIZE_STRING);
+    $yearOfRelease = filter_var($newAlbumData['yearOfRelease'], FILTER_SANITIZE_NUMBER_INT);
+    $albumArtworkURL = filter_var($newAlbumData['albumArtworkURL'], FILTER_SANITIZE_URL);
+    $rating = filter_var($newAlbumData['rating'], FILTER_SANITIZE_NUMBER_INT);
 
     if (validateNewAlbumData(
-        $newAlbumData['albumName'],
-        $newAlbumData['artistName'],
-        $newAlbumData['yearOfRelease'],
-        $newAlbumData['albumArtworkURL'],
-        $newAlbumData['rating'])
+        $albumName,
+        $artistName,
+        $yearOfRelease,
+        $albumArtworkURL,
+        $rating)
     ) {
         //Add to DB
         $db = fetchDb();
         insertNewAlbum(
-            $newAlbumData['albumName'],
-            $newAlbumData['artistName'],
-            $newAlbumData['yearOfRelease'],
-            $newAlbumData['albumgit ArtworkURL'],
-            $newAlbumData['rating'],
+            $albumName,
+            $artistName,
+            $yearOfRelease,
+            $albumArtworkURL,
+            $rating,
             $db
         );
     }

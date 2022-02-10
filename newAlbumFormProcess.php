061f496cbc6f66cafa1836e0fc28c2fbae1acd09
Luke Grayland <lgrayland96@gmail.com>
2022-02-10 09:47:14 +0000

    created validation and sanitisation for new album form, next step is to write input to db query

--- newAlbumFormProcess.php
+++ newAlbumFormProcess.php
@@ -8,13 +8,12 @@ if (checkAlbumDataExists($newAlbumData)) {
     //Sanitise
     $newAlbumData['albumName'] = filter_var($newAlbumData['albumName'], FILTER_SANITIZE_STRING);
     $newAlbumData['artistName'] = filter_var($newAlbumData['artistName'], FILTER_SANITIZE_STRING);
-    $newAlbumData['yearOfRelease'] = filter_var($newAlbumData['artistName'], FILTER_SANITIZE_NUMBER_INT);
-    $newAlbumData['albumArtworkURL'] = filter_var($newAlbumData['artistName'], FILTER_SANITIZE_URL);
+    $newAlbumData['yearOfRelease'] = filter_var($newAlbumData['yearOfRelease'], FILTER_SANITIZE_NUMBER_INT);
+    $newAlbumData['albumArtworkURL'] = filter_var($newAlbumData['albumArtworkURL'], FILTER_SANITIZE_URL);
     $newAlbumData['rating'] = filter_var($newAlbumData['rating'], FILTER_SANITIZE_NUMBER_INT);
 
-
     if (
-        //Validate (Some of this is truthy, some isn't - still trying to figure out why)
+        //Validate
         strlen($newAlbumData['albumName']) < 255 &&
         strlen($newAlbumData['artistName']) < 255 &&
         $newAlbumData['yearOfRelease'] > 1000 &&
@@ -23,14 +22,15 @@ if (checkAlbumDataExists($newAlbumData)) {
         $newAlbumData['rating'] < 11 &&
         $newAlbumData['rating'] > 0
     ) {
-        /* Add to DB */
+        //Add to DB
+        echo 'The next step is to now add this data to the database, for now I\m echoing it to show it works...' . '<br>';
 
         echo '<pre>';
         var_dump($newAlbumData);
         echo '<pre>';
 
     } else {
-        echo 'error';
+        echo 'Some of your data was invalid, please try again.';
     }
 }
 
